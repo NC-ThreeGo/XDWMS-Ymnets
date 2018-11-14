61881030e2c81b9014d00db51d51ed5b84b09881
zipiii <111.aaa>
2018-11-14 10:11:19 +0800

    创建项目文件，进行初始化

---
+++ src/CodeCreate/Code/Server.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeCreate.Code
+{
+    public class Server
+    {
+        private string serverName;
+
+        public string ServerName
+        {
+            get { return serverName; }
+            set { serverName = value; }
+        }
+
+        private string dbName;
+
+        public string DbName
+        {
+            get { return dbName; }
+            set { dbName = value; }
+        }
+
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(password))
+            {
+                return "server=" + serverName + ";database=" + dbName + ";uid=" + userName + ";pwd=" + password;
+            }
+            else
+            {
+                return "server=" + serverName + ";database=" + dbName + ";Integrated Security=true";
+            }
+        }
+    }
+}
---
+++ src/CodeCreate/Core/Core.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using CommonData.Data.Core;
+using CommonData.Data.Core.SQLCore;
+
+namespace CodeCreate.Core
+{
+    public static class Core
+    {
+        public static IList<string> GetSource()
+        {
+            DataTable tableDataSouce=SqlClientFactory.Instance.CreateDataSourceEnumerator().GetDataSources();
+            IList<string> listServerName = new List<string>();
+            for (int i = 0; i < tableDataSouce.Rows.Count; i++)
+            {
+                listServerName.Add(tableDataSouce.Rows[i]["ServerName"].ToString()+"\\"+tableDataSouce.Rows[i]["InstanceName"].ToString());
+            }
+            return listServerName;
+        }
+
+
+        public static DataTable GetDataBase(string servername, string userid, string password)
+        {
+            using (IDbProvider provider = new SqlProvider())
+            {
+                if (string.IsNullOrEmpty(userid) && string.IsNullOrEmpty(password))
+                {
+                    provider.ConnectionString = "server=" + servername + ";database=master;Integrated Security=true";
+                }
+                else
+                {
+                    provider.ConnectionString = "server=" + servername + ";database=master;uid=" + userid + ";pwd=" + password;
+                }
+                IBaseHelper baseHelper = new BaseHelper();
+                string sql="select name from sysdatabases where dbid>4";
+                return baseHelper.ExecuteTable(provider,sql);
+            }
+        }
+
+        public static DataTable GetTable(string servername, string databasename, string userid, string password)
+        {
+            using (IDbProvider provider = new SqlProvider())
+            {
+                if (string.IsNullOrEmpty(userid) && string.IsNullOrEmpty(password))
+                {
+                    provider.ConnectionString = "server=" + servername + ";database="+databasename+";Integrated Security=true";
+                }
+                else
+                {
+                    provider.ConnectionString = "server=" + servername + ";database="+databasename+";uid=" + userid + ";pwd=" + password;
+                }
+                IBaseHelper baseHelper = new BaseHelper();
+                string sql = "select name from sysobjects where type='U'";
+                return baseHelper.ExecuteTable(provider, sql);
+            }
+        }
+    }
+}
