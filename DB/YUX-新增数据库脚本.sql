51dc8134643749047cb0be7a761384bcb4580d0a
yux1211 <yux121@126.com>
2018-11-24 11:43:46 +0800

    基础数据表结构更新，并把库存，库位的表名进行了更改

--- DB/YUX-新增数据库脚本.sql
+++ DB/YUX-新增数据库脚本.sql
@@ -1,4 +1,4 @@
-USE [XDWMS]
+﻿USE [XDWMS]
 GO
 
 /****** Object:  Table [dbo].[WMS_Part]    Script Date: 2018/11/23 11:37:35 ******/
@@ -71,5 +71,264 @@ GO
 
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Part', @level2type=N'COLUMN',@level2name=N'ModifyTime'
 GO
+
+USE [XDWMS]
+GO
+
+/****** Object:  Table [dbo].[WMS_Supplier]    Script Date: 2018/11/24 10:48:05 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[WMS_Supplier](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SupplierCode] [nvarchar](50) NOT NULL,
+	[SupplierShortName] [nvarchar](50) NULL,
+	[SupplierName] [nvarchar](100) NOT NULL,
+	[SupplierType] [nvarchar](10) NULL,
+	[LinkMan] [nvarchar](20) NULL,
+	[LinkManTel] [nvarchar](20) NULL,
+	[LinkManAddress] [nvarchar](100) NULL,
+	[Status] [nvarchar](10) NULL,
+	[Remark] [nvarchar](500) NULL,
+	[CreatePerson] [nvarchar](50) NULL,
+	[CreateTime] [datetime] NULL,
+	[ModifyPerson] [nvarchar](50) NULL,
+	[ModifyTime] [datetime] NULL
+) ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'供应商ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'供应商编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'SupplierCode'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'供应商简称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'SupplierShortName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'供应商名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'SupplierName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'供应商类型' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'SupplierType'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'联系人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'LinkMan'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'联系人电话' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'LinkManTel'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'联系人地址' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'LinkManAddress'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'状态' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'Status'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'说明' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'Remark'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'CreatePerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'CreateTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'ModifyPerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Supplier', @level2type=N'COLUMN',@level2name=N'ModifyTime'
+GO
+
+
+USE [XDWMS]
+GO
+
+/****** Object:  Table [dbo].[WMS_Customer]    Script Date: 2018/11/24 10:48:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[WMS_Customer](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerCode] [nvarchar](20) NOT NULL,
+	[CustomerShortName] [nvarchar](20) NULL,
+	[CustomerName] [nvarchar](50) NOT NULL,
+	[CustomerType] [nvarchar](20) NULL,
+	[LinkMan] [nvarchar](20) NULL,
+	[LinkManTel] [nvarchar](20) NULL,
+	[LinkManAddress] [nvarchar](100) NULL,
+	[Status] [nvarchar](10) NULL,
+	[Remark] [nvarchar](200) NULL,
+	[CreatePerson] [nvarchar](50) NULL,
+	[CreateTime] [datetime] NULL,
+	[ModifyPerson] [nvarchar](50) NULL,
+	[ModifyTime] [datetime] NULL,
+ CONSTRAINT [PK_WMS_Customer] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'客户ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'客户编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'CustomerCode'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'客户简称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'CustomerShortName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'客户名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'CustomerName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'客户类型' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'CustomerType'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'联系人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'LinkMan'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'联系电话' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'LinkManTel'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'联系地址' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'LinkManAddress'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'状态' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'Status'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'说明' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'Remark'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'CreatePerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'CreateTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'ModifyPerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_Customer', @level2type=N'COLUMN',@level2name=N'ModifyTime'
+GO
+
+USE [XDWMS]
+GO
+
+/****** Object:  Table [dbo].[WMS_InvInfo]    Script Date: 2018/11/24 11:28:27 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[WMS_InvInfo](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[InvCode] [nvarchar](20) NOT NULL,
+	[InvName] [nvarchar](50) NOT NULL,
+	[Remark] [nchar](10) NULL,
+	[Status] [nchar](10) NULL,
+	[CreatePerson] [nvarchar](50) NULL,
+	[CreateTime] [datetime] NULL,
+	[ModifyPerson] [nvarchar](50) NULL,
+	[ModifyTime] [datetime] NULL
+) ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'库房ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'库房编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'InvCode'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'库房名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'InvName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'说明' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'Remark'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'状态' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'Status'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'CreatePerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'CreateTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'ModifyPerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_InvInfo', @level2type=N'COLUMN',@level2name=N'ModifyTime'
+GO
+
+USE [XDWMS]
+GO
+
+/****** Object:  Table [dbo].[WMS_SubInvInfo]    Script Date: 2018/11/24 11:38:47 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[WMS_SubInvInfo](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SubInvCode] [nvarchar](20) NOT NULL,
+	[SubInvName] [nvarchar](50) NOT NULL,
+	[InvCode] [nvarchar](20) NOT NULL,
+	[Status] [nvarchar](10) NULL,
+	[Remark] [nvarchar](100) NULL,
+	[CreatePerson] [nvarchar](50) NULL,
+	[CreateTime] [datetime] NULL,
+	[ModifyPerson] [nvarchar](50) NULL,
+	[ModifyTime] [datetime] NULL
+) ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'库位ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'库位编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'SubInvCode'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'库位名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'SubInvName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'库房编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'InvCode'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'状态' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'Status'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'说明' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'Remark'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'CreatePerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'CreateTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'ModifyPerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WMS_SubInvInfo', @level2type=N'COLUMN',@level2name=N'ModifyTime'
+GO
+
+
+
+
+
+
+
+
+
+
 
 
