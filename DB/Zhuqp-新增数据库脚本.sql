40cd53c636d7caed918f4a9b75ee2ccc2b5f1909
zipiii <111.aaa>
2018-12-05 16:13:30 +0800

    数据库脚本

--- DB/Zhuqp-新增数据库脚本.sql
+++ DB/Zhuqp-新增数据库脚本.sql
@@ -42,3 +42,59 @@ GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'导入用户' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysImportExcelLog', @level2type=N'COLUMN',@level2name=N'CreateBy'
 GO
 
+
+
+
+
+
+/****** Object:  Table [dbo].[SysParam]    Script Date: 2018/12/5 14:10:58 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SysParam](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[TypeCode] [nvarchar](20) NULL,
+	[TypeName] [nvarchar](50) NULL,
+	[ParamCode] [nvarchar](50) NULL,
+	[ParamName] [nvarchar](50) NULL,
+	[CreatePerson] [nvarchar](50) NULL,
+	[CreateTime] [datetime] NULL,
+	[ModifyPerson] [nvarchar](50) NULL,
+	[ModifyTime] [datetime] NULL,
+ CONSTRAINT [PK_SysParam] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'参数类别编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'TypeCode'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'参数类别名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'TypeName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'参数值编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'ParamCode'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'参数值名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'ParamName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'CreatePerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'CreateTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改人' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'ModifyPerson'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'修改时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SysParam', @level2type=N'COLUMN',@level2name=N'ModifyTime'
+GO
+
+
+
