ec6f9b3668d7aea8540cadb966148eaa9a988746
yux1211 <yux121@126.com>
2018-12-21 16:31:18 +0800

    物料送检更改

--- src/Apps.Web/Areas/WMS/Controllers/AIController.cs
+++ src/Apps.Web/Areas/WMS/Controllers/AIController.cs
@@ -30,16 +30,24 @@ namespace Apps.Web.Areas.WMS.Controllers
         [SupportFilter]
         public ActionResult Index()
         {
+            //定义送检状态下拉框的值
+            List<ReportType> InspTypes = new List<ReportType>();
+            InspTypes.Add(new ReportType() { Type = 0, Name = "" });
+            InspTypes.Add(new ReportType() { Type = 1, Name = "未送检" });
+            InspTypes.Add(new ReportType() { Type = 2, Name = "已检验" });
+            ViewBag.InspStatus = new SelectList(InspTypes, "Name", "Name");
+
             return View();
         }
         [HttpPost]
         [SupportFilter(ActionName="Index")]
-        public JsonResult GetList(GridPager pager, string po, string supplierShortName, string partCode, DateTime beginDate, DateTime endDate)
+        public JsonResult GetList(GridPager pager, string po, string supplierShortName, string partCode, DateTime beginDate, DateTime endDate,string inspectStatus)
         {
             //TODO:显示到货的到货单
             //List<WMS_AIModel> list = m_BLL.GetList(ref pager, queryStr);
-            List<WMS_AIModel> list = m_BLL.GetListByWhere(ref pager, "WMS_PO.PO.Contains(\"" + po + "\") && WMS_PO.WMS_Supplier.SupplierShortName.Contains(\""
-               + supplierShortName + "\")&& WMS_PO.WMS_Part.PartCode.Contains(\"" + partCode + "\")&& ArrivalDate>=(\""
+            
+            List <WMS_AIModel> list = m_BLL.GetListByWhere(ref pager, "WMS_PO.PO.Contains(\"" + po + "\") && WMS_PO.WMS_Supplier.SupplierShortName.Contains(\""
+               + supplierShortName + "\")&& WMS_PO.WMS_Part.PartCode.Contains(\"" + partCode + "\")&& InspectStatus.Contains(\"" + inspectStatus + "\")&& ArrivalDate>=(\""
                + beginDate + "\")&& ArrivalDate<=(\"" + endDate + "\")");
             GridRows <WMS_AIModel> grs = new GridRows<WMS_AIModel>();
             grs.rows = list;
@@ -204,45 +212,51 @@ namespace Apps.Web.Areas.WMS.Controllers
             }
         }
         [SupportFilter]
-        public ActionResult Export(string queryStr)
+        public ActionResult Export(string po, string supplierShortName, string partCode, DateTime beginDate, DateTime endDate)
         {
-            List<WMS_AIModel> list = m_BLL.GetList(ref setNoPagerAscById, queryStr);
+            //List<WMS_AIModel> list = m_BLL.GetList(ref setNoPagerAscById, queryStr);
+            List<WMS_AIModel> list = m_BLL.GetListByWhere(ref setNoPagerAscById, "WMS_PO.PO.Contains(\"" + po + "\") && WMS_PO.WMS_Supplier.SupplierShortName.Contains(\""
+               + supplierShortName + "\")&& WMS_PO.WMS_Part.PartCode.Contains(\"" + partCode + "\")&& ArrivalDate>=(\""
+               + beginDate + "\")&& ArrivalDate<=(\"" + endDate + "\")");
             JArray jObjects = new JArray();
                 foreach (var item in list)
                 {
                     var jo = new JObject();
-                    jo.Add("Id", item.Id);
+                    //jo.Add("Id", item.Id);
                     jo.Add("到货单据号", item.ArrivalBillNum);
-                    jo.Add("采购订单ID", item.POId);
-                    jo.Add("物料ID", item.PartId);
-                    jo.Add("到货箱数", item.BoxQty);
+                    jo.Add("采购订单", item.PO);
+                    jo.Add("物料编码", item.PartCode);
+                    jo.Add("物料名称", item.PartName);
+                    jo.Add("供应商简称", item.SupplierShortName);
                     jo.Add("到货数量", item.ArrivalQty);
+                    jo.Add("到货箱数", item.BoxQty);
+                    jo.Add("计划到货日期", item.PlanDate);
                     jo.Add("到货日期", item.ArrivalDate);
                     jo.Add("接收人", item.ReceiveMan);
                     jo.Add("到货状态", item.ReceiveStatus);
-                    jo.Add("送检单号", item.InspectBillNum);
-                    jo.Add("送检人", item.InspectMan);
-                    jo.Add("送检日期", item.InspectDate);
-                    jo.Add("送检状体", item.InspectStatus);
-                    jo.Add("检验日期", item.CheckOutDate);
-                    jo.Add("检验结果", item.CheckOutResult);
-                    jo.Add("合格数量", item.QualifyQty);
-                    jo.Add("不合格数量", item.NoQualifyQty);
-                    jo.Add("检验说明", item.CheckOutRemark);
-                    jo.Add("重新送检单", item.ReInspectBillNum);
-                    jo.Add("入库单号", item.InStoreBillNum);
-                    jo.Add("InStoreMan", item.InStoreMan);
-                    jo.Add("入库仓库", item.InvId);
-                    jo.Add("入库状态", item.InStoreStatus);
-                    jo.Add("Attr1", item.Attr1);
-                    jo.Add("Attr2", item.Attr2);
-                    jo.Add("Attr3", item.Attr3);
-                    jo.Add("Attr4", item.Attr4);
-                    jo.Add("Attr5", item.Attr5);
-                    jo.Add("创建人", item.CreatePerson);
-                    jo.Add("创建时间", item.CreateTime);
-                    jo.Add("修改人", item.ModifyPerson);
-                    jo.Add("修改时间", item.ModifyTime);
+                    //jo.Add("送检单号", item.InspectBillNum);
+                    //jo.Add("送检人", item.InspectMan);
+                    //jo.Add("送检日期", item.InspectDate);
+                    jo.Add("送检状态", item.InspectStatus);
+                    //jo.Add("检验日期", item.CheckOutDate);
+                    //jo.Add("检验结果", item.CheckOutResult);
+                    //jo.Add("合格数量", item.QualifyQty);
+                    //jo.Add("不合格数量", item.NoQualifyQty);
+                    //jo.Add("检验说明", item.CheckOutRemark);
+                    //jo.Add("重新送检单", item.ReInspectBillNum);
+                    //jo.Add("入库单号", item.InStoreBillNum);
+                    //jo.Add("InStoreMan", item.InStoreMan);
+                    //jo.Add("入库仓库", item.InvId);
+                    //jo.Add("入库状态", item.InStoreStatus);
+                    //jo.Add("Attr1", item.Attr1);
+                    //jo.Add("Attr2", item.Attr2);
+                    //jo.Add("Attr3", item.Attr3);
+                    //jo.Add("Attr4", item.Attr4);
+                    //jo.Add("Attr5", item.Attr5);
+                    //jo.Add("创建人", item.CreatePerson);
+                    //jo.Add("创建时间", item.CreateTime);
+                    //jo.Add("修改人", item.ModifyPerson);
+                    //jo.Add("修改时间", item.ModifyTime);
                     jObjects.Add(jo);
                 }
                 var dt = JsonConvert.DeserializeObject<DataTable>(jObjects.ToString());
