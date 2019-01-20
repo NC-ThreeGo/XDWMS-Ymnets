cac52ae021ce9acfd0f969d5563feec6e6e1f387
zipiii <111.aaa>
2019-01-20 15:18:49 +0800

    修改WMS_Inventory_H的InventoryTime字段

--- src/Apps.Models/WMS_Inventory_D.cs
+++ src/Apps.Models/WMS_Inventory_D.cs
@@ -34,9 +34,9 @@ namespace Apps.Models
         public string ModifyPerson { get; set; }
         public Nullable<System.DateTime> ModifyTime { get; set; }
     
-        public virtual WMS_Inventory_H WMS_Inventory_H { get; set; }
         public virtual WMS_InvInfo WMS_InvInfo { get; set; }
         public virtual WMS_Part WMS_Part { get; set; }
         public virtual WMS_SubInvInfo WMS_SubInvInfo { get; set; }
+        public virtual WMS_Inventory_H WMS_Inventory_H { get; set; }
     }
 }
